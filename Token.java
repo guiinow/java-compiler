c516a11c97b8bddf5336a36a6971f5e67aca39f8
Breno Rotte <114703393+BrenoRotte@users.noreply.github.com>
2024-11-26 14:15:28 -0300

    Update Token.java

--- Token.java
+++ Token.java
@@ -22,3 +22,35 @@ public class Token {
         return "Token: " + token + " Type: " + type;
     }
 }
+
+
+###############################################################################################
+
+public class Token {
+
+    // conferir se o código abaixo é válido
+    
+    private String token;
+    private EnumToken type; //Mudei String para EnumToken para evitar erros de digitação
+
+    public Token(String token, EnumToken type) {
+        // Validação para evitar valores nulos
+        if (token == null || type == null) {
+            throw new IllegalArgumentException("Token e Type não podem ser nulos.");
+        }
+        this.token = token;
+        this.type = type;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public EnumToken getType() {
+        return type;
+    }
+
+    public String toString() {
+        return "Token: " + token + " Type: " + type;
+    }
+}
