dac2c375e672a5cf29fb00c8f8e2176677cb7ad1
Guilherme Ferreira <guifs2@hotmail.com>
2025-03-10 16:33:27 -0300

    add @Override no Interp e comenta linha 96 do lang.cup

--- lang/ast/visitors/Interp.java
+++ lang/ast/visitors/Interp.java
@@ -36,6 +36,7 @@ public class Interp extends LVisitor {
     varTable.put(id, value);
   }
 
+  @Override
   public void visit(Program p) {
     for (Data data : p.getData()) {
       data.accept(this);
@@ -45,7 +46,7 @@ public class Interp extends LVisitor {
       func.accept(this);
     }
   }
-
+  @Override
   public void visit(PlusOperator e) {
     e.getLeft().accept(this);
     e.getRight().accept(this);
@@ -60,7 +61,7 @@ public class Interp extends LVisitor {
       stk.push(r);
     }
   };
-
+  @Override
   public void visit(MultOperator e) {
     e.getLeft().accept(this);
     e.getRight().accept(this);
@@ -76,7 +77,7 @@ public class Interp extends LVisitor {
       stk.push(r);
     }
   };
-
+  @Override
   public void visit(MinusOperator e) {
     e.getLeft().accept(this);
     e.getRight().accept(this);
@@ -92,7 +93,7 @@ public class Interp extends LVisitor {
       stk.push(r);
     }
   }
-
+  @Override
   public void visit(UnaryMinusOperator e) {
     e.getExp().accept(this);
 
@@ -106,7 +107,7 @@ public class Interp extends LVisitor {
       throw new RuntimeException("Unary '-' applied to non-number: " + value);
     }
   }
-
+  @Override
   public void visit(DivOperator e) {
     e.getLeft().accept(this);
     e.getRight().accept(this);
@@ -117,7 +118,7 @@ public class Interp extends LVisitor {
     float r = left / right;
     stk.push(r);
   }
-
+  @Override
   public void visit(ModOperator e) {
     e.getLeft().accept(this);
     e.getRight().accept(this);
@@ -128,7 +129,7 @@ public class Interp extends LVisitor {
     float r = left % right;
     stk.push(r);
   }
-
+  @Override
   public void visit(GtOperator e) {
     e.getLeft().accept(this);
     e.getRight().accept(this);
@@ -139,7 +140,7 @@ public class Interp extends LVisitor {
     boolean r = left > right;
     stk.push(r);
   }
-
+  @Override
   public void visit(LtOperator e) {
     e.getLeft().accept(this);
     e.getRight().accept(this);
@@ -150,7 +151,7 @@ public class Interp extends LVisitor {
     boolean r = left < right;
     stk.push(r);
   }
-
+  @Override
   public void visit(EqOperator e) {
     e.getLeft().accept(this);
     e.getRight().accept(this);
@@ -158,7 +159,7 @@ public class Interp extends LVisitor {
     boolean r = ((Number) stk.pop()).floatValue() == ((Number) stk.pop()).floatValue();
     stk.push(r);
   }
-
+  @Override
   public void visit(NeqOperator e) {
     e.getLeft().accept(this);
     e.getRight().accept(this);
@@ -166,7 +167,7 @@ public class Interp extends LVisitor {
     boolean r = ((Number) stk.pop()).floatValue() != ((Number) stk.pop()).floatValue();
     stk.push(r);
   }
-
+  @Override
   public void visit(AndOperator e) {
     e.getLeft().accept(this);
     boolean left = (Boolean) stk.pop();
@@ -177,7 +178,7 @@ public class Interp extends LVisitor {
     boolean r = left && right;
     stk.push(r);
   }
-
+  @Override
   public void visit(NotOperator e) {
     e.getRight().accept(this);
     boolean right = (Boolean) stk.pop();
@@ -185,19 +186,19 @@ public class Interp extends LVisitor {
     boolean r = !right;
     stk.push(r);
   }
-
+  @Override
   public void visit(LParen d) {
   }
-
+  @Override
   public void visit(RParen d) {
   }
-
+  @Override
   public void visit(LBracket d) {
   }
-
+  @Override
   public void visit(RBracket d) {
   }
-
+  @Override
   public void visit(Attrib e) {
     // e.getRight().accept(this);
     // Var left = e.getLeft();
@@ -207,7 +208,7 @@ public class Interp extends LVisitor {
 
     // stk.push(right);
   }
-
+  @Override
   public void visit(AssignCmd c) {
     // Avalia a expressão do lado direito
     c.getExp().accept(this);
@@ -221,7 +222,7 @@ public class Interp extends LVisitor {
     // Empilha o valor novamente (opcional, dependendo da semântica da linguagem)
     stk.push(value);
   }
-
+  @Override
   public void visit(Var e) {
     // Obtém o valor da variável da tabela
     Object value = getVarValue(e.getName());
@@ -229,162 +230,162 @@ public class Interp extends LVisitor {
     // Empilha o valor na pilha
     stk.push(value);
   }
-
+  @Override
   public void visit(IntLit e) {
     stk.push(e.getValue());
   };
-
+  @Override
   public void visit(BoolLit e) {
     stk.push(e.getValue());
   };
-
+  @Override
   public void visit(NullLit e) {
     stk.push(e.getValue());
   };
-
+  @Override
   public void visit(TyBool t) {
     System.out.println("Tipo: " + t.getTypeName());
     stk.push(t.getTypeName());
   };
-
+  @Override
   public void visit(TyInt t) {
     System.out.println("Tipo: " + t.getTypeName());
     stk.push(t.getTypeName());
   }
-
+  @Override
   public void visit(Type t) {
     System.out.println("Tipo: " + t.getTypeName());
     stk.push(t.getTypeName());
   }
-
+  @Override
   public void visit(TyId t) {
     System.out.println("Tipo: " + t.getTypeName());
     stk.push(t.getTypeName());
   }
-
+  @Override
   public void visit(TyFloat t) {
     System.out.println("Tipo: " + t.getTypeName());
     stk.push(t.getTypeName());
   }
-
+  @Override
   public void visit(TyChar t) {
     System.out.println("Tipo: " + t.getTypeName());
     stk.push(t.getTypeName());
   }
-
+  @Override
   public void visit(TyNull t) {
     System.out.println("Tipo: " + t.getTypeName());
     stk.push(t.getTypeName());
   }
-
+  @Override
   public void visit(FunDef p) {
     stk.push(p.getParams());
     stk.push(p.getBody());
     stk.push(p.getRetrn());
   }
-
+  @Override
   public void visit(Bind p) {
     stk.push(p.getVar());
     stk.push(p.getType());
   }
-
+  @Override
   public void visit(Decl p) {
     stk.push(p.getFieldName());
     stk.push(p.getFieldType());
   }
-
+  @Override
   public void visit(Block p) {
     for (Cmd cmd : p.getCommands()) {
       cmd.accept(this);
     }
     stk.push(p.getCommands());
   }
-
+  @Override
   public void visit(DotOperator o) {
   }
-
+  @Override
   public void visit(Cmd o) {
 
   }
-
+  @Override
   public void visit(StmtBlock o) {
 
   }
-
+  @Override
   public void visit(ReadCmd p) {
 
   }
-
+  @Override
   public void visit(PrintCmd p) {
 
   }
-
+  @Override
   public void visit(ReturnCmd p) {
 
   }
-
+  @Override
   public void visit(ArrayAccess p) {
 
   }
-
+  @Override
   public void visit(FieldAccess p) {
 
   }
-
+  @Override
   public void visit(LValue p) {
 
   }
-
+  @Override
   public void visit(FloatLit o) {
     stk.push(o.getValue());
   }
-
+  @Override
   public void visit(CharLit o) {
     stk.push(o.getValue());
   }
-
+  @Override
   public void visit(Comma d) {
   }
-
+  @Override
   public void visit(LBrace d) {
   }
-
+  @Override
   public void visit(RBrace d) {
   }
-
+  @Override
   public void visit(Semicolon d) {
   }
-
+  @Override
   public void visit(Data r) {
     stk.push(r.getFields());
     stk.push(r.getTypeName());
   }
-
+  @Override
   public void visit(Param d) {
   }
-
+  @Override
   public void visit(If r) {
   }
-
+  @Override
   public void visit(IfElse r) {
 
   }
-
+  @Override
   public void visit(Iterate r) {
   }
-
+  @Override
   public void visit(Print r) {
   }
-
+  @Override
   public void visit(Read r) {
   }
-
+  @Override
   public void visit(Res r) {
   }
-
+  @Override
   public void visit(Return r) {
   }
-
+  @Override
   public void visit(EOF e) {
   };
 
